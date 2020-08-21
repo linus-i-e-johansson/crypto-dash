ebc11e14652c1a8a48cb0c48de1177f14010c559
Linus-Johansson <40174194+Linus-Johansson@users.noreply.github.com>
2020-08-21 18:55:25 +0200

    add appbar and grid

--- src/app/index.js
+++ src/app/index.js
@@ -3,12 +3,13 @@ import './App.css';
 import styled,{css} from "styled-components";
 import AppLayout from "./AppLayout";
 import WelcomeMessage from "./WelcomeMessage";
-
+import AppBar from "./AppBar";
 
 class Index extends Component{
   render(){
     return (
         <AppLayout>
+          <AppBar />
           <WelcomeMessage />
         </AppLayout>
 
