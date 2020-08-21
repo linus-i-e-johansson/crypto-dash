e80fe00817bb2d44f768e67258f48c14c4e5f158
Linus-Johansson <40174194+Linus-Johansson@users.noreply.github.com>
2020-08-22 00:12:21 +0200

    added small changes

--- src/app/AppBar.js
+++ src/app/AppBar.js
@@ -1,18 +1,40 @@
 import React from "react";
-import styled from "styled-components";
+import styled,{css} from "styled-components";
+
+const Logo = styled.div`
+    font-size: 1.5em;
+`
 
 const Bar = styled.div`
     display:grid;
     grid-template-columns:180px auto 100px 100px;
-    
-
 `
 
+const ControlButtonElem = styled.div`
+    cursor:pointer;
+    ${props => props.active && css`
+        color: blue;
+    `}
+`
+
+
+function ControlButton ({name, active}){
+    return(
+        <ControlButtonElem active={active}>
+            {name}
+        </ControlButtonElem>
+    )
+}
+
 export default function (){
-    return <Bar>
-        <div>CryptoDash</div>
-        <div />
-        <div>Dashboard</div>
-        <div>Settings</div>
-    </Bar>
+    return(
+        <Bar>
+            <div>CryptoDash</div>
+            <div />
+            <ControlButton active name={"Dashboard"}/>
+            <ControlButton name={"Settings"} />
+        </Bar>
+
+
+    )
 }
